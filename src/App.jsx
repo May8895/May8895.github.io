f4fee354b7a0a93b6e4675b00b50fc3d6b5e9e33
May Thazin Myo <u6511335@au.edu>
2024-07-12 16:34:07 +0700

    add index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,4 @@
 
-// import { useState } from 'react'
-// import './App.css'
-
-// function App() {
-
-//   // var a = 1
-//   const [vat, setVat] = useState(0)
-//   const[gross , setgross]= useState(0)
-//   const [discount, setDiscount] = useState(0)
-
-
-
-
-  
-//   function handle(e , discount) {
-//     let p = e.target.value
-//     console.log(p)
-//     let d = e.target.value
-//     console.log
-//     let g = p - d
-//     let v = g * 0.07
-//     setVat(v.toFixed(2))
-//     setgross(g.toFixed(2))
-//     setDiscount(d.toFixed(2))
-
-//   }
-  
-
-//   return (
-//     <>
-//       <p style={{fontSize: '30pt'}}>Price:
-//         <input type="number" 
-//           style={{fontSize: '30pt'}} 
-//           onChange={handle} />
-//       </p>
-//       <br />
-//       <p style={{fontSize: '30pt'}}>Discount:
-//         <input type="number" 
-//           style={{fontSize: '30pt'}} 
-//           onChange={handle(p,discount)} />
-//       </p>
-//       <h1>GROSS PRICE = {gross}</h1>
-//       <h1>VAT = {vat}</h1>
-//     </>
-//   )
-// }
-// import { useState } from 'react'
-// import './App.css'
-
-// function App() {
-
-//   const [vat, setVat] = useState(0)
-//   const [gross, setGross] = useState(0)
-//   const [discount, setDiscount] = useState(0)
-//   const [price, setPrice] = useState(0)
-
-//   function handle(e) {
-//     const { name, value } = e.target;
-//     if (name === 'price') {
-//       setPrice(parseFloat(value));
-//     } else if (name === 'discount') {
-//       setDiscount(parseFloat(value));
-//     }
-//   }
-
-//   function calculate() {
-//     let g = price - discount;
-//     let v = g * 0.07;
-//     setVat(v.toFixed(2));
-//     setGross(g.toFixed(2));
-//   }
-
-//   return (
-//     <>
-//       <p style={{fontSize: '30pt'}}>Price:
-//         <input type="number" 
-//           name="price"
-//           style={{fontSize: '30pt'}} 
-//           onChange={handle} />
-//       </p>
-//       <br />
-//       <p style={{fontSize: '30pt'}}>Discount:
-//         <input type="number" 
-//           name="discount"
-//           style={{fontSize: '30pt'}} 
-//           onChange={handle} />
-//       </p>
-//       <br />
-//       {/* <button onClick={calculate} style={{fontSize: '20pt'}}>Calculate</button> */}
-//       <h1>GROSS PRICE = {gross}</h1>
-//       <h1>VAT = {vat}</h1>
-//     </>
-//   )
-// }
-
-// export default App
 import { useState } from 'react'
 import './App.css'
 
@@ -137,7 +41,7 @@ function App() {
           style={{fontSize: '30pt'}} 
           onChange={handle} />
       </p>
-      <h1>GROSS PRICE = {gross}</h1>
+      <h1>GROSS PRICE  = {gross}</h1>
       <h1>VAT = {vat}</h1>
     </>
   )
